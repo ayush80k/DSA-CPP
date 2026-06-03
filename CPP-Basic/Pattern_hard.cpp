39fbe0f12ea6a44938fd8426fb7fa3d90e2b1418
Ayush Tank <ayushtank191@gmail.com>
2026-06-03 13:26:55 +0530

    Added a C++ Code for printing a complex pattern.

---
+++ CPP-Basic/Pattern_hard.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4, size = 2*n-1, dist_top,dist_bot,dist_left,dist_rig,val;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            dist_top = i;
+            dist_bot = size-i-1;
+            dist_left = j;
+            dist_rig = size-j-1;
+            val = n - min(min(dist_top,dist_bot),min(dist_left,dist_rig));
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+/*
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4 
+4 3 2 2 2 3 4 
+4 3 3 3 3 3 4 
+4 4 4 4 4 4 4 
+*/
